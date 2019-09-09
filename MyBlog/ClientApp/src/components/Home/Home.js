e0eef5b7238b41d1fba3dd761cc62bd72ea9b054
Aidan Harris <aidanharris1001@gmail.com>
2019-09-09 22:27:21 +1200

    Removed the links to the fetch-data and counter pages

--- MyBlog/ClientApp/src/components/Home/Home.js
+++ MyBlog/ClientApp/src/components/Home/Home.js
@@ -8,11 +8,6 @@ export class Home extends Component {
       <div>
         <h1>Welcome to Aidan's Devsite!</h1>
         <p>Here is where I show off all my projects, aspirations and stuff</p>
-        <ul>
-          <li><a href='/counter'>This is a link to the counter page! I haven't made any other interesting page yet</a></li>
-          <li><a href='/fetch-data'>And here we have a link to the fetch page, nothing great here either!</a></li>
-          <li><a href='/'>This is a link to the page that we're currently on, not much point clicking here</a></li>
-        </ul>
         <p>So what are my plans for this website?</p>
         <ul>
           <li>Create a page to display my achievements!</li>
