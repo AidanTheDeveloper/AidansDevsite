8a15d94598154fe6d274fd854326c317788956d7
Aidan Harris <aidanharris1001@gmail.com>
2019-09-10 17:01:04 +1200

    Added setupTests.js file to configure Enzyme for tests

--- MyBlog/ClientApp/src/App.test.js
+++ MyBlog/ClientApp/src/App.test.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
-import Enzyme, { mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16'
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
-  Enzyme.configure({adapter: new Adapter()})
   const wrapper = mount(<MemoryRouter><App /></MemoryRouter>)
 });
